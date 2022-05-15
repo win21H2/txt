0bfae4a1a836345e69e36870801668518241b7b5
win_21H2 <92825997+win21H2@users.noreply.github.com>
2022-05-14 20:14:04 -0500

    init commit and code cleanup

--- txt/Views/SettingsPage.xaml.cs
+++ txt/Views/SettingsPage.xaml.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-
 using txt.ViewModels;
-
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-namespace txt.Views
-{
+namespace txt.Views {
     // TODO: Change the URL for your privacy policy in the Resource File, currently set to https://YourPrivacyUrlGoesHere
-    public sealed partial class SettingsPage : Page
-    {
+    public sealed partial class SettingsPage : Page {
         public SettingsViewModel ViewModel { get; } = new SettingsViewModel();
 
-        public SettingsPage()
-        {
+        public SettingsPage() {
             InitializeComponent();
         }
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
-        {
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
             await ViewModel.InitializeAsync();
         }
     }
