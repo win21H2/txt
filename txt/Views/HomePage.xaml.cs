16e38b811fa247d69d5eb91e68a912d237eb32f1
win_21H2 <92825997+win21H2@users.noreply.github.com>
2022-05-15 09:41:03 -0500

    commit NOTE: added a .txt file opener which although does not display text, shows what file was selected

--- txt/Views/HomePage.xaml.cs
+++ txt/Views/HomePage.xaml.cs
@@ -5,12 +5,12 @@ using txt.ViewModels;
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace txt.Views {
     public sealed partial class HomePage : Page {
-
         public HomeViewModel ViewModel { get; } = new HomeViewModel();
-        public IStorageFile sampleFile { get; private set; }
 
         public HomePage() {
             InitializeComponent();
@@ -21,24 +21,22 @@ namespace txt.Views {
         private void Home_Click(object sender, RoutedEventArgs e) {
             this.Frame.Navigate(typeof(HomePage));
         }
+        private void Save_Click(object sender, RoutedEventArgs e) {
 
+        }
 
-        
-        private async void Save_Click(object sender, RoutedEventArgs e) {
-            Windows.Storage.StorageFolder storageFolder =
-            Windows.Storage.ApplicationData.Current.LocalFolder;
-            Windows.Storage.StorageFile sampleFile =
-                await storageFolder.GetFileAsync("sample.txt");
-        }
-        private async void Create_Click(object sender, RoutedEventArgs e) {
-            Windows.Storage.StorageFolder storageFolder =
-            Windows.Storage.ApplicationData.Current.LocalFolder;
-            Windows.Storage.StorageFile sampleFile =
-                await storageFolder.CreateFileAsync("sample.txt",
-                    Windows.Storage.CreationCollisionOption.ReplaceExisting);
-        }
-        private async void Write_Click(object sender, RoutedEventArgs e) {
-            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, "Swift as a shadow");
+        private async void buttonfilepick_Click(object sender, RoutedEventArgs e) {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".txt");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null) {
+                TextBlockoutput.Text = "User Selected File:" + file.Name;
+            }
+            else {
+                TextBlockoutput.Text = "Try Again";
+            }
         }
     }
 }
