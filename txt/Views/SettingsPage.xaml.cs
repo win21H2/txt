9cffc3feca9f689cbd9a70a0bfd21aae2cd6c3aa
324Hz <1024bitcoins@gmail.com>
2022-05-17 12:31:04 -0500

    commit

--- txt/Views/SettingsPage.xaml.cs
+++ txt/Views/SettingsPage.xaml.cs
@@ -17,5 +17,18 @@ namespace txt.Views {
         private void BackButton_Click(object sender, RoutedEventArgs e) {
             App.TryGoBack();
         }
+
+        private void PasswordBox1_PasswordChanged(object sender, RoutedEventArgs e) {
+            // Cast the sender as a PasswordBox
+            PasswordBox passwordBox = sender as PasswordBox;
+            // Get the PasswordBox inputted string length
+            int length = passwordBox.Password.Length;
+            if (length > 0) {
+                Psswrd.Text = length + " Character(s).";
+            }
+            else {
+                Psswrd.Text = "The password entry box is empty.";
+            }
+        }
     }
 }
