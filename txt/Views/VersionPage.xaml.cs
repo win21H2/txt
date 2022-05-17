f5ebf9eeb12a3905e59de9e976ffeb955c6cedf2
324Hz <1024bitcoins@gmail.com>
2022-05-17 07:54:40 -0500

    commit

--- txt/Views/VersionPage.xaml.cs
+++ txt/Views/VersionPage.xaml.cs
@@ -35,18 +35,26 @@ namespace txt.Views {
         private void Version_Click(object sender, RoutedEventArgs e) {
             this.Frame.Navigate(typeof(VersionPage));
         }
-        private async void V0111_Click(object sender, RoutedEventArgs e) {
+        private async void V01010_Click(object sender, RoutedEventArgs e) {
             var dialog = new Windows.UI.Popups.MessageDialog(
-                        "v1.1.1.1 \n \n \n \n Notes \n \n Added the settings page, home page, about page, and version page\nStarted working on the text editor format and layout",
+                        "v0.1.0.10\n\n\n\nNotes\n\n-Added the settings page, home page, about page, and version page\n-Started working on the text editor format and layout",
                         "e67b1ab1-9ddf-428f-ba77-434c2b34134e");
             dialog.Commands.Add(new Windows.UI.Popups.UICommand("Close") { Id = 1 });
             dialog.CancelCommandIndex = 1;
             var result = await dialog.ShowAsync();
         }
-        private async void V0112_Click(object sender, RoutedEventArgs e) {
+        private async void V01011_Click(object sender, RoutedEventArgs e) {
             var dialog = new Windows.UI.Popups.MessageDialog(
-                        "v1.1.1.2 \n \n \n \n Notes \n \n Cleaned up the code for the text editor and the main frame of the application\nWorked on the wrapping of the words for all of the pages",
+                        "v0.1.0.11\n\n\n\nNotes\n\n-Cleaned up the code for the text editor and the main frame of the application\n-Worked on the wrapping of the words for all of the pages",
                         "d15a5f61-9a3d-4896-858b-ea52ae382867");
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("Close") { Id = 1 });
+            dialog.CancelCommandIndex = 1;
+            var result = await dialog.ShowAsync();
+        }
+        private async void V01012_Click(object sender, RoutedEventArgs e) {
+            var dialog = new Windows.UI.Popups.MessageDialog(
+                        "v0.1.0.12\n\n\n\nNotes\n\n-Changed the overall background theme for the application\n-Changed the button layout on the top menu so that Home is on the top left",
+                        "6388ff56-8fff-4a3e-8eed-3eb3746004bc");
             dialog.Commands.Add(new Windows.UI.Popups.UICommand("Close") { Id = 1 });
             dialog.CancelCommandIndex = 1;
             var result = await dialog.ShowAsync();
