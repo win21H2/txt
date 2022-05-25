47250ab5ba6592ff4484af9f9f07bb135b5de650
324Hz <1024bitcoins@gmail.com>
2022-05-25 12:17:25 -0500

    commit

--- txt/Views/HomePage.xaml.cs
+++ txt/Views/HomePage.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 using txt.ViewModels;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace txt.Views {
     public sealed partial class HomePage : Page {
+        const string TEXT_FILE_NAME = "SampleTextFile.txt";
         public HomeViewModel ViewModel { get; } = new HomeViewModel();
 
         public HomePage() {
@@ -34,6 +40,55 @@ namespace txt.Views {
             dialog.PrimaryButtonText = "Ok";
             dialog.DefaultButton = ContentDialogButton.Primary;
             var result = await dialog.ShowAsync();
+        }
+
+        private async void readFile_Click(object sender, RoutedEventArgs e) {
+            string str = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
+            textBlock.Text = "You entered \n\n" + str;
+        }
+
+        private async void writeFile_Click(object sender, RoutedEventArgs e) {
+            string textFilePath = await FileHelper.WriteTextFile(TEXT_FILE_NAME, textbox.Text);
+        }
+
+    }
+
+    public static class FileHelper {
+        public static async Task<string>
+           WriteTextFile(string filename, string contents) {
+
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile textFile = await localFolder.CreateFileAsync(filename,
+               CreationCollisionOption.ReplaceExisting);
+
+            using (IRandomAccessStream textStream = await
+               textFile.OpenAsync(FileAccessMode.ReadWrite)) {
+
+                using (DataWriter textWriter = new DataWriter(textStream)) {
+                    textWriter.WriteString(contents);
+                    await textWriter.StoreAsync();
+                }
+            }
+
+            return textFile.Path;
+        }
+
+        public static async Task<string> ReadTextFile(string filename) {
+            string contents;
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile textFile = await localFolder.GetFileAsync(filename);
+
+            using (IRandomAccessStream textStream = await textFile.OpenReadAsync()) {
+
+                using (DataReader textReader = new DataReader(textStream)) {
+                    uint textLength = (uint)textStream.Size;
+                    await textReader.LoadAsync(textLength);
+                    contents = textReader.ReadString(textLength);
+                }
+
+            }
+
+            return contents;
         }
     }
 }
--- txt/Views/TestPage.xaml.cs
+++ txt/Views/TestPage.xaml.cs
@@ -17,57 +17,5 @@ namespace txt.Views {
         private void Home_Click(object sender, RoutedEventArgs e) {
             this.Frame.Navigate(typeof(HomePage));
         }
-
-
-        
-
-        private async void readFile_Click(object sender, RoutedEventArgs e) {
-            string str = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
-            textBlock.Text = str;
-        }
-
-        private async void writeFile_Click(object sender, RoutedEventArgs e) {
-            string textFilePath = await FileHelper.WriteTextFile(TEXT_FILE_NAME, textBox.Text);
-        }
-
-    }
-
-    public static class FileHelper {
-        public static async Task<string>
-           WriteTextFile(string filename, string contents) {
-
-            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-            StorageFile textFile = await localFolder.CreateFileAsync(filename,
-               CreationCollisionOption.ReplaceExisting);
-
-            using (IRandomAccessStream textStream = await
-               textFile.OpenAsync(FileAccessMode.ReadWrite)) {
-
-                using (DataWriter textWriter = new DataWriter(textStream)) {
-                    textWriter.WriteString(contents);
-                    await textWriter.StoreAsync();
-                }
-            }
-
-            return textFile.Path;
-        }
-
-        public static async Task <string> ReadTextFile(string filename) {
-            string contents;
-            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-            StorageFile textFile = await localFolder.GetFileAsync(filename);
-
-            using (IRandomAccessStream textStream = await textFile.OpenReadAsync()) {
-
-                using (DataReader textReader = new DataReader(textStream)) {
-                    uint textLength = (uint)textStream.Size;
-                    await textReader.LoadAsync(textLength);
-                    contents = textReader.ReadString(textLength);
-                }
-
-            }
-
-            return contents;
-        }
     }
 }
