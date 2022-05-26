e8e2d35d3509efa029dec80b3a55249053ef2b10
324Hz <1024bitcoins@gmail.com>
2022-05-26 12:29:40 -0500

    commit

--- txt/Views/HomePage.xaml.cs
+++ txt/Views/HomePage.xaml.cs
@@ -3,10 +3,8 @@ using System.Threading.Tasks;
 using txt.ViewModels;
 using Windows.Storage;
 using Windows.Storage.Streams;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 
 namespace txt.Views {
     public sealed partial class HomePage : Page {
@@ -22,9 +20,6 @@ namespace txt.Views {
         private void Version_Click(object sender, RoutedEventArgs e) {
             this.Frame.Navigate(typeof(VersionPage));
         }
-        private void Test_Click(object sender, RoutedEventArgs e) {
-            this.Frame.Navigate(typeof(TestPage));
-        }
 
         private async void Home_Click(object sender, RoutedEventArgs e) {
             ContentDialog dialog = new ContentDialog();
@@ -34,21 +29,15 @@ namespace txt.Views {
             var result = await dialog.ShowAsync();
         }
 
-        private async void Save_Click(object sender, RoutedEventArgs e) {
-            ContentDialog dialog = new ContentDialog();
-            dialog.Title = "Feature coming soon!";
-            dialog.PrimaryButtonText = "Ok";
-            dialog.DefaultButton = ContentDialogButton.Primary;
-            var result = await dialog.ShowAsync();
-        }
-
         private async void readFile_Click(object sender, RoutedEventArgs e) {
             string str = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
             textBlock.Text = "You entered \n\n" + str;
+            // Work on reading the file from a different folder
         }
 
         private async void writeFile_Click(object sender, RoutedEventArgs e) {
             string textFilePath = await FileHelper.WriteTextFile(TEXT_FILE_NAME, textbox.Text);
+            // Work on trying to write to an external folder other than the apps' local folder
         }
     }
 
@@ -62,7 +51,6 @@ namespace txt.Views {
 
             using (IRandomAccessStream textStream = await
                textFile.OpenAsync(FileAccessMode.ReadWrite)) {
-
                 using (DataWriter textWriter = new DataWriter(textStream)) {
                     textWriter.WriteString(contents);
                     await textWriter.StoreAsync();
@@ -77,7 +65,6 @@ namespace txt.Views {
             StorageFile textFile = await localFolder.GetFileAsync(filename);
 
             using (IRandomAccessStream textStream = await textFile.OpenReadAsync()) {
-
                 using (DataReader textReader = new DataReader(textStream)) {
                     uint textLength = (uint)textStream.Size;
                     await textReader.LoadAsync(textLength);
