752b76fa3444b64b3c48742f0c2976476640e0c6
324Hz <1024bitcoins@gmail.com>
2022-05-27 11:25:15 -0500

    commit

--- txt/Views/HomePage.xaml.cs
+++ txt/Views/HomePage.xaml.cs
@@ -33,6 +33,36 @@ namespace txt.Views {
             string str = await FileHelper.ReadTextFile(TEXT_FILE_NAME);
             textBlock.Text = "You entered \n\n" + str;
             // Work on reading the file from a different folder
+
+            // Check here: https://docs.microsoft.com/en-us/windows/communitytoolkit/helpers/storagefiles
+
+            // NOTE This must be used from an async function
+            // string myText = "Great information that the users wants to keep";
+            // StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            // Save some text to a file named appFilename.txt (in the local cache folder)
+            // var storageFile = await StorageFileHelper.WriteTextToLocalCacheFileAsync(myText, "appFilename.txt");
+
+            // Load some text from a file named appFilename.txt in the local cache folder 
+            // string loadedText = await StorageFileHelper.ReadTextFromLocalCacheFileAsync("appFilename.txt");
+
+            // Save some text to a file named appFilename.txt (in the local folder)
+            // storageFile = await StorageFileHelper.WriteTextToLocalFileAsync(myText, "appFilename.txt");
+
+            // Load some text from a file named appFilename.txt in the local folder 
+            // loadedText = await StorageFileHelper.ReadTextFromLocalFileAsync("appFilename.txt");
+
+            // Check if a file exists in a specific folder
+            // bool exists = await localFolder.FileExistsAsync("appFilename.txt");
+
+            // Check if a file exists in a specific folder or in one of its subfolders
+            // bool exists = await localFolder.FileExistsAsync("appFilename.txt", true);
+
+            // Check if a file name is valid or not
+            // bool isFileNameValid = StorageFileHelper.IsFileNameValid("appFilename.txt");
+
+            // Check if a file path is valid or not
+            // bool isFilePathValid = StorageFileHelper.IsFilePathValid("folder/appFilename.txt");
         }
 
         private async void writeFile_Click(object sender, RoutedEventArgs e) {
